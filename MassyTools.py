98d4420546faffb89b99c352d4b5128b9c099f1e
Bas Jansen <tarskin@gmail.com>
2015-12-01 11:46:00 +0100

    Copyrighting
    Added copyright information to the source code

--- MassyTools.py
+++ MassyTools.py
@@ -1,4 +1,20 @@
 #! /usr/bin/env python
+#
+# Copyright 2014-2015 Bas C. Jansen
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# You should have received a coyp of the Apache 2.0 license along
+# with this program; if not, see 
+# http://www.apache.org/licenses/LICENSE-2.0
+
 from datetime import datetime
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from scipy.interpolate import interp1d
@@ -603,8 +619,7 @@ class App():
 					   "The software can both be ran in single spectrum\n"
 					   "and batch processing modes. The software can perform\n"
 					   "data calibration, data extraction and quality score\n"
-					   "calculations (a modification of the Nicolardi et al,\n"
-					   "2010 publication).\n\n"
+					   "calculations.\n\n"
 					   "This software is released under the Apache 2.0 License,\n"
 					   "Full details regarding this license can be found at\n"
 					   "the following URL:\n\n"
